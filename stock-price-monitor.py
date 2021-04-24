62da247d112f985546841951cfe229d65eba5c9d
mayursrt <mayurmachhi19@gmail.com>
2021-04-25 03:38:35 +0530

    code

--- stock-price-monitor.py
+++ stock-price-monitor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import yfinance as yf
-import streamlit as stl
+import streamlit as st
 
-stl.write("""
+st.write("""
 # Stock Price Monitor
 
 Shown below are the stock closing price and volume of Google.
@@ -18,5 +18,5 @@ tickerData = yf.Ticker(tickerSymbol)
 #Get historical data prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 
-stl.line_chart(tickerDf.Close)
-stl.line_chart(tickerDf.Volume)
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)

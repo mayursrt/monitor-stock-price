6aa7ef7707eca6dbfc454b7ca0906fc0e4f1ba0e
mayursrt <mayurmachhi19@gmail.com>
2021-04-25 04:15:31 +0530

    code

--- stock-price-monitor.py
+++ stock-price-monitor.py
@@ -9,14 +9,16 @@ Shown below are the stock closing price and volume of Google.
 
 """)
 
+tickr = st.selectbox('Select', ['GOOGL','AAPL','AMZN','ACN'])
+
 #Ticker symbol
-tickerSymbol = 'GOOGL' #You can change the ticker symbol for the name of the stock of the company you're intrested in.
+tickerSymbol = tickr #You can change the ticker symbol for the name of the stock of the company you're intrested in.
 
 
 #Data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #Get historical data prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-4-24')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
